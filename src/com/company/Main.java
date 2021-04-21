6ddb66acc08a45142ba71bf77e92829e95483eee
Richard <richard.yang.tong@gmail.com>
2021-04-21 08:58:07 -0400

    Added a character.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
         byte age = 30;
         long viewsCount = 3_123_456_789L;
         float price = 10.99F;
-        System.out.println(price);
+        char letter = 'A';
+        System.out.println(letter);
     }
 }
