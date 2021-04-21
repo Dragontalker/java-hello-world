5e3b0e969f9fbdf265e66646079e122abf969717
Richard <richard.yang.tong@gmail.com>
2021-04-21 09:22:37 -0400

    Adde a simple array.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,13 @@
 package com.company;
 
-import java.util.Locale;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        String message = "  Hello World" + "!!  \"Richard\"";
-        System.out.println(message.endsWith("!!"));
-        System.out.println(message.startsWith("!!"));
-        System.out.println(message.length());
-        System.out.println(message.indexOf("H"));
-        System.out.println(message.indexOf("sky"));
-        System.out.println(message.replace("!", "*"));
-        System.out.println(message);
-        System.out.println(message.toLowerCase(Locale.ROOT));
-        System.out.println(message.trim());
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        System.out.println(Arrays.toString(numbers));
     }
 }
