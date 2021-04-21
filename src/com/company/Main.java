006b47b399876dfcecd319089315db75a7bcbdf6
Richard <richard.yang.tong@gmail.com>
2021-04-21 10:20:36 -0400

    More examples on increment operation.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         int x = 1;
-        x++;
+        int y = x++;
+        int z = ++x;
         System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
     }
 }
