cac969d8fbc34250c5f67c77af1d8e5c85a73c0b
Richard <richard.yang.tong@gmail.com>
2021-04-21 10:19:02 -0400

    Added an increment operation.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        double result = (double)10 / (double)3;
-        System.out.println(result);
+        int x = 1;
+        x++;
+        System.out.println(x);
     }
 }
