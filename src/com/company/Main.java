bc0ceff9e5b231a408f64554da9f5defdeaf3ecc
Richard <richard.yang.tong@gmail.com>
2021-04-21 10:31:27 -0400

    Added example for ceiling and flooring.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int result = Math.round(1.1F);
-        System.out.println(result);
+        double result1 = Math.ceil(1.1F);
+        double result2 = Math.floor(1.1F);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
