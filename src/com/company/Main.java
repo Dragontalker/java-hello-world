adc156153f5e6d7fb0946a075d6196eeacad74e4
Richard <richard.yang.tong@gmail.com>
2021-04-21 08:47:41 -0400

    Added a variable with reference.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int age = 30;
-        age = 25;
-        System.out.println(age);
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 }
